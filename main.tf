61ab68f3bc08ea6b7e24245160e67e0f0cb56c06
Ramiro Núñez Sánchez <ramironunezsanchez3@gmail.com>
2023-11-15 18:43:27 -0800

    changes

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 module "s3" {
-  source = "./S3"
+  source     = "./S3"
+  bucket_name = var.s3_bucket_name
 }
 
 module "cloudwatch" {
-  source = "./CloudWatch"
+  source           = "./CloudWatch"
+  log_group_name   = var.cloudwatch_log_group_name
 }
 
 module "sqs" {
-  source = "./SQS"
+  source     = "./SQS"
+  queue_name = var.sqs_queue_name
 }
 
 module "sns" {
-  source = "./SNS"
+  source      = "./SNS"
+  topic_name  = var.sns_topic_name
 }
