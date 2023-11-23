c79d32a0be7e6159fb61c319b6171d9ccce62f92
Ramiro Núñez Sánchez <ramironunezsanchez3@gmail.com>
2023-11-22 21:35:40 -0800

    ec2 and cloudwatch log group created

--- main.tf
+++ main.tf
@@ -10,3 +10,20 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+resource "aws_instance" "example_server" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["ssh-virginia-sg", "http-virginia-sg"]
+  key_name               = "ramiro-virginia"
+
+  tags = {
+    Owner = "Ramiro"
+    Name = "example_instance_terraform"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "example_log_group" {
+  name = "/example/log/group/terraform"
+  retention_in_days = 7  
+}
