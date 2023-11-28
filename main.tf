a77fbf3126f26e64a01427f4435a1e5f201a0efe
Ramiro Núñez Sánchez <ramironunezsanchez3@gmail.com>
2023-11-27 18:56:57 -0800

    sns topic

--- main.tf
+++ main.tf
@@ -98,3 +98,13 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   runtime       = "python3.8"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
+
+resource "aws_sns_topic" "example_sns" {
+  name = "test-ramiro"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.example_sns.arn
+  protocol  = "email"
+  endpoint  = "ramironunezsanchez3@gmail.com"
+}
