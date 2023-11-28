5721731101adc15fea2a0fc7f3192790983adfa7
Ramiro Núñez Sánchez <ramironunezsanchez3@gmail.com>
2023-11-27 18:23:05 -0800

    python and iam roles policies

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example_server" {
+resource "aws_instance" "example_instance" {
   ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["ssh-virginia-sg", "http-virginia-sg"]
@@ -28,11 +28,73 @@ resource "aws_cloudwatch_log_group" "example_log_group" {
   retention_in_days = 7
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "example_bucket" {
   bucket = "ramiro-test-bucket"
 
   tags = {
     Name  = "Ramiro Bucket"
     Owner = "Ramiro"
   }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "Test_Lambda_Function_Role_Ramiro"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "aws_iam_policy_for_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Test_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
