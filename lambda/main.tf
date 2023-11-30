871b6bf2ea81fe63c3e4545cfa59321bd2f22a2d
Ramiro Núñez Sánchez <ramironunezsanchez3@gmail.com>
2023-11-29 19:53:24 -0800

    nombre lambda

--- lambda/main.tf
+++ lambda/main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 resource "aws_lambda_function" "terraform_lambda_func" {
   filename      = "${path.module}/python/hello-python.zip"
-  function_name = "Test_Lambda_Function"
+  function_name = "actualizarS3"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
